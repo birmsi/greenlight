7b35a440cf316c2b87594a55789650bc4dcfe503
birmsi <birmsi@gmail.com>
2023-08-01 13:49:43 +0100

    initial skeleton and beginning of json

---
+++ cmd/api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+
+	var config config
+
+	flag.IntVar(&config.port, "port", 3000, "API server port")
+	flag.StringVar(&config.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: config,
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", config.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+
+}
